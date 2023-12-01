1575d47b62d03616f2999cc7d954b0fbde41d530
baskar <baskar15082000@gmail.com>
2023-12-01 12:19:39 +0530

    call backs

--- fs-problem2.cjs
+++ fs-problem2.cjs
@@ -12,75 +12,71 @@ function fsProblem2() {
   }
   readFile("lipsum.txt", function cb(file) {
     const upper = file.toUpperCase();
-    fs.writeFile("newFile_1.txt", upper, (err, data) => {
+    fs.writeFile("Upper.txt", upper, (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        console.log("newFile_1.txt ctreated");
-        fs.writeFile("fileNames.txt", "newFile_1.txt", (err) => {
+        console.log("Upper.txt ctreated");
+        fs.writeFile("fileNames.txt", "Upper.txt", (err) => {
           if (err) {
             console.error(err);
           } else {
-            console.log("newFile_1.txt writtern");
-            readFile("newFile_1.txt", function (data1) {
+            console.log("Upper.txt writtern");
+            readFile("Upper.txt", function (data1) {
               const lower = data1.toLowerCase();
               const sentence = lower.replaceAll(".", ".\n");
-              fs.writeFile("newFile_2.txt", sentence, (err) => {
+              fs.writeFile("lower.txt", sentence, (err) => {
                 if (err) {
                   console.error(err);
                 } else {
-                  console.log("newFile_2.txt created");
-                  fs.appendFile(
-                    "fileNames.txt",
-                    "\nnewFile_2.txt",
-                    (err, data) => {
-                      if (err) {
-                        console.error(err);
-                      } else {
-                        console.log("newFile_2.txt writtern");
-                        readFile("newFile_2.txt", function (data2) {
-                          const sort = data2.split(".").sort().join("");
+                  console.log("lower.txt created");
+                  fs.appendFile("fileNames.txt", "\nlower.txt", (err, data) => {
+                    if (err) {
+                      console.error(err);
+                    } else {
+                      console.log("lower.txt writtern");
+                      readFile("lower.txt", function (data2) {
+                        const sort = data2.split(".").sort().join("");
 
-                          fs.writeFile("newFile_3.txt", sort, (err) => {
-                            if (err) {
-                              console.error(err);
-                            } else {
-                              console.log("newFile_3.txt created");
-                              fs.appendFile(
-                                "fileNames.txt",
-                                "\nnewFile_3.txt",
-                                (err, data3) => {
-                                  if (err) {
-                                    return;
-                                  } else {
-                                    console.log("newFile_3.txt writtern");
-                                    readFile("fileNames.txt", function (data4) {
-                                      if (err) {
-                                        console.error(err);
-                                      } else {
-                                        const files = data4.split("\n");
-                                        files.forEach((element) => {
-                                          fs.unlink(element, (err) => {
-                                            if (err) {
-                                              return;
-                                            } else {
-                                              console.log(
-                                                element + "" + "deleted"
-                                              );
-                                            }
-                                          });
+                        fs.writeFile("sort.txt", sort, (err) => {
+                          if (err) {
+                            console.error(err);
+                          } else {
+                            console.log("sort.txt created");
+                            fs.appendFile(
+                              "fileNames.txt",
+                              "\nsort.txt",
+                              (err, data3) => {
+                                if (err) {
+                                  return;
+                                } else {
+                                  console.log("sort.txt writtern");
+                                  readFile("fileNames.txt", function (data4) {
+                                    if (err) {
+                                      console.error(err);
+                                    } else {
+                                      const files = data4.split("\n");
+                                      files.forEach((element) => {
+                                        fs.unlink(element, (err) => {
+                                          if (err) {
+                                            return;
+                                          } else {
+                                            console.log(
+                                              element + "" + "deleted"
+                                            );
+                                          }
                                         });
-                                      }
-                                    });
-                                  }
+                                      });
+                                    }
+                                  });
                                 }
-                              );
-                            }
-                          });
+                              }
+                            );
+                          }
                         });
-                      }
+                      });
                     }
-                  );
+                  });
                 }
               });
             });
